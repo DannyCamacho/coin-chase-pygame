8ab4f70e91a1147a6b806120ac385c0e35281547
Daniel Camacho <dcamacho4@saddleback.edu>
2024-03-01 00:51:56 -0800

    add launch screen, game over screen, and player attack

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pygame.font
+
 from sprites import *
 from config import *
 import sys
@@ -6,12 +8,18 @@ import sys
 class Game:
     def __init__(self):
         pygame.init()
+
         self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.running = True
+        self.font = pygame.font.Font('assets/general_font.ttf', 32)
 
         self.character_spritesheet = Spritesheet('img/character.png')
+        self.enemy_spritesheet = Spritesheet('img/enemy.png')
+        self.attack_spritesheet = Spritesheet('img/attack.png')
         self.terrain_spritesheet = Spritesheet('img/terrain.png')
+        self.intro_background = pygame.image.load('img/introbackground.png')
+        self.game_over_background = pygame.image.load('img/gameover.png')
 
     def create_tilemap(self):
         for i, row in enumerate(tilemap):
@@ -19,8 +27,10 @@ class Game:
                 Ground(self, j, i)
                 if col == "B":
                     Block(self, j, i)
+                if col == "E":
+                    Enemy(self, j, i)
                 if col == "P":
-                    Player(self, j, i)
+                    self.player = Player(self, j, i)
                 if col == ".":
                     pass
 
@@ -39,6 +49,16 @@ class Game:
             if event.type == pygame.QUIT:
                 self.playing = False
                 self.running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    if self.player.facing == 'up':
+                        Attack(self, self.player.rect.x, self.player.rect.y - TILESIZE)
+                    if self.player.facing == 'down':
+                        Attack(self, self.player.rect.x, self.player.rect.y + TILESIZE)
+                    if self.player.facing == 'left':
+                        Attack(self, self.player.rect.x - TILESIZE, self.player.rect.y)
+                    if self.player.facing == 'right':
+                        Attack(self, self.player.rect.x + TILESIZE, self.player.rect.y)
 
     def update(self):  # game loop updates
         self.all_sprites.update()
@@ -54,13 +74,64 @@ class Game:
             self.events()
             self.update()
             self.draw()
-        self.running = False
 
     def game_over(self):
-        pass
+        title = self.font.render('Game Over', True, WHITE)
+        title_rect = title.get_rect(center=(WIN_WIDTH / 2, WIN_HEIGHT / 2))
+
+        restart_button = Button(10, WIN_HEIGHT - 120, 120, 50, WHITE, BLACK, 'Restart', 32)
+        quit_button = Button(10, WIN_HEIGHT - 60, 120, 50, WHITE, BLACK, 'Quit', 32)
+
+        for sprite in self.all_sprites:
+            sprite.kill()
+
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            mouse_pos = pygame.mouse.get_pos()
+            mouse_pressed = pygame.mouse.get_pressed()
+
+            if restart_button.is_pressed(mouse_pos, mouse_pressed):
+                self.new()
+                self.main()
+
+            if quit_button.is_pressed(mouse_pos, mouse_pressed):
+                self.running = False
+
+            self.screen.blit(self.game_over_background, (0, 0))
+            self.screen.blit(title, title_rect)
+            self.screen.blit(restart_button.image, restart_button.rect)
+            self.screen.blit(quit_button.image, quit_button.rect)
+            self.clock.tick(FPS)
+            pygame.display.update()
 
     def intro_screen(self):
-        pass
+        intro = True
+
+        title = self.font.render('Pygame RPG', True, BLACK)
+        title_rect = title.get_rect(center=(WIN_WIDTH / 2, WIN_HEIGHT / 2))
+
+        play_button = Button(10, WIN_HEIGHT - 60, 120, 50, WHITE, BLACK, 'Play', 32)
+
+        while intro:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    intro = False
+                    self.running = False
+
+            mouse_pos = pygame.mouse.get_pos()
+            mouse_pressed = pygame.mouse.get_pressed()
+
+            if play_button.is_pressed(mouse_pos, mouse_pressed):
+                intro = False
+
+            self.screen.blit(self.intro_background, (0, 0))
+            self.screen.blit(title, title_rect)
+            self.screen.blit(play_button.image, play_button.rect)
+            self.clock.tick(FPS)
+            pygame.display.update()
 
 
 g = Game()
@@ -69,6 +140,5 @@ g.new()
 while g.running:
     g.main()
     g.game_over()
-
 pygame.quit()
 sys.exit()
